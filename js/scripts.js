5f5b0ff563c7046b7bdffd09c4934af42a736f30
connorbacon9 <cbacon4@vcd-ch204-22.eastern.ewu.edu>
2016-10-19 15:47:06 -0700

    Maybe Fixed Masonry

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,10 @@ $('h1').lettering();
 // Make backstretch work
 
 $.backstretch('/nameplate/img/sunrise.png');
+
+$('.tiles').masonry({
+  // options
+  itemSelector: 'section',
+  columnWidth: 0,
+  gutter: 5
+});
